7e11fcd5615d97b4c6feda24fde0d45fec5f85f1
Luis Garcia <lalejandro.1988@gmail.com>
2021-05-25 18:10:38 -0600

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 25 }}>Hola Wicho</Text>
+    </View>
+  )
+}
+
+export default App;
